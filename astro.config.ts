109e6c78755128a5b78140ea3c4edc58e0af8464
Mayank <mayank99@users.noreply.github.com>
2022-08-13 20:14:21 -0400

    Get basic form working

--- astro.config.ts
+++ astro.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from "astro/config";
 import solidJs from "@astrojs/solid-js";
 
+// @ts-ignore
+import netlify from "@astrojs/netlify/edge-functions";
+
 // https://astro.build/config
 export default defineConfig({
 	integrations: [solidJs()],
+	output: "server",
+	adapter: netlify(),
 });
