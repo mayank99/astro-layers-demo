02c5ff1a6d5d81630017df226b18eba1563b2d5f
Mayank <mayank99@users.noreply.github.com>
2022-08-16 14:22:44 -0400

    Remove solid

--- astro.config.ts
+++ astro.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "astro/config";
-import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [solidJs()],
+	integrations: [],
 });
